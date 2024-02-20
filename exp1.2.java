30509b3925a3bbdd53112dfdd379c18ad114d596
beanand47 <157506680+beanand47@users.noreply.github.com>
2024-02-20 19:45:46 +0530

    Update exp1.2.java

--- exp1.2.java
+++ exp1.2.java
@@ -1,5 +1,6 @@
 public class exp1.2{
   public static void main(string[]args){
-    system.out.println("hello world");
+    system.out.println("hello everyone");
+    system.out.println("i am Anand Tiwari");
       }
 }
